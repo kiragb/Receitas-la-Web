73abfd2735235365555230552b279789c909a5e6
Natalia <nataliadiasfernande@gmail.com>
2024-10-27 15:21:20 -0300

    adicionando pagina de criação de receita em php

--- docs/php-servidor/servicos/ReceitaServico.php
+++ docs/php-servidor/servicos/ReceitaServico.php
@@ -59,19 +59,88 @@ class ReceitaServico
         foreach ($dados as $dado) {
             $receitas[] = $this->gerarReceita($dado);
         }
-        
+
         return $receitas;
+    }
+
+    public function criarReceita($dado, $imagem)
+    {
+        $receita = $this->gerarReceita($dado);
+        $receita->imagem_url = "/img/$imagem";
+        $receita->ingredientes = explode("\n", $_POST["ingredientes"]);
+        $receita->modo_de_preparo = explode("\n", $_POST["etapas"]);
+
+        try {
+            $this->conn->beginTransaction();
+
+            $sql = "INSERT INTO receita (titulo, minutos, porcao, imagem) VALUES (:titulo, :minutos, :porcao, :imagem);";
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->bindParam(":titulo", $receita->titulo_receita);
+            $stmt->bindParam(":minutos", $receita->minutos_para_preparo);
+            $stmt->bindParam(":porcao", $receita->porcoes);
+            $stmt->bindParam(":imagem", $receita->imagem_url);
+            $stmt->execute();
+
+            $id = $this->conn->lastInsertId();
+
+            $sql = "INSERT INTO ingredientes (receita_id, ingrediente) VALUES (:receita_id, :ingrediente);";
+            $stmt = $this->conn->prepare($sql);
+            foreach ($receita->ingredientes as $ingrediente) {
+                $stmt->bindParam(":receita_id", $id);
+                $stmt->bindParam(":ingrediente", $ingrediente);
+                $stmt->execute();
+            }
+
+            $sql = "INSERT INTO modo_de_preparo (receita_id, etapa) VALUES (:receita_id, :etapa);";
+            $stmt = $this->conn->prepare($sql);
+            foreach ($receita->modo_de_preparo as $etapa) {
+                $stmt->bindParam(":receita_id", $id);
+                $stmt->bindParam(":etapa", $etapa);
+                $stmt->execute();
+            }
+
+            $this->conn->commit();
+            return true;
+        } catch (PDOException $ex) {
+            $this->conn->rollBack();
+            return false;
+        }
+    }
+
+    public function salvarImagem($imagem)
+    {
+        $target_dir = "../img/";
+        $target_file = $target_dir . basename($imagem["name"]);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        $check = getimagesize($imagem["tmp_name"]);
+        if ($check === false) {
+            die("File is not an image.");
+        }
+
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            die("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+        }
+
+        if (!move_uploaded_file($imagem["tmp_name"], $target_file)) {
+            die("Sorry, there was an error uploading your file.");
+        }
+        return $imagem["name"];
     }
 
     private function gerarReceita($dado)
     {
         $receita = new Receita();
 
-        $receita->id = $dado["id"];
+        $receita->id = array_key_exists("id", $dado) ? $dado["id"] : 0;
         $receita->titulo_receita = $dado["titulo"];
         $receita->minutos_para_preparo = $dado["minutos"];
         $receita->porcoes = $dado["porcao"];
-        $receita->imagem_url = $dado["imagem"];
+        $receita->imagem_url = array_key_exists("imagem", $dado) ? $dado["imagem"] : "";
         $receita->ingredientes = [];
         $receita->modo_de_preparo = [];
 
